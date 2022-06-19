1ede2d76281a2fde2ce57cd11998fec310c34b70
umekishimaru <riki12344649@gmail.com>
2022-06-19 11:18:39 +0900

    tab-menu

---
+++ js/main.js
@@ -0,0 +1,36 @@
+{
+
+  const menuItems =document.querySelectorAll('.menu li a');
+  const contents =document.querySelectorAll('.content');
+menuItems.forEach((clickedItem)=>{
+  clickedItem.addEventListener('click',(e)=>
+  {e.preventDefault();
+
+
+
+ menuItems.forEach(item =>{
+  item.classList.remove('active');
+});
+   
+  clickedItem.classList.add('active');
+
+  contents.forEach(content =>{
+    content.classList.remove('active');
+  })
+  
+document.getElementById(clickedItem.dataset.a).classList.add('active')
+
+});
+ });
+
+
+
+
+
+
+
+
+
+}
+
+
